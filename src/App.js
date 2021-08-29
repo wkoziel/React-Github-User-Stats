9b3479fc6daed04b6b50f525e75106461ba1420c
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-08-29 18:50:02 +0200

    Login and Error pages finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Dashboard, Login, Error } from './pages'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 function App() {
-  return <h1>App</h1>
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path='/'>
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='*'>
+            <Error />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
