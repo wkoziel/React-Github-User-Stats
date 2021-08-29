f1668d2e0e23c12b68db8e08070cb85126b01026
Wojciech Kozieł <wojtek98koziel@gmail.com>
2021-08-29 19:26:01 +0200

    Context updated with states

--- src/context/context.js
+++ src/context/context.js
@@ -1,9 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import mockUser from './mockData.js/mockUser'
+import mockRepos from './mockData.js/mockRepos'
+import mockFollowers from './mockData.js/mockFollowers'
+import axios from 'axios'
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={'hi'}>{children}</AppContext.Provider>
+  const [githubUser, setGithubUser] = useState(mockUser)
+  const [githubRepos, setGithubRepos] = useState(mockRepos)
+  const [githubFollowers, setGithubFollowers] = useState(mockFollowers)
+  return (
+    <AppContext.Provider value={{ githubUser, githubFollowers, githubRepos }}>
+      {children}
+    </AppContext.Provider>
+  )
 }
 
 export const useGlobalConstext = () => {
